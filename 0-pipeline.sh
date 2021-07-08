83ceb88f54a6bf08980be2740d122a1cd0d53911
Linas Vepštas <linasvepstas@gmail.com>
2021-07-08 21:31:09 +0000

    Start trying to configure stuff.

--- 0-pipeline.sh
+++ 0-pipeline.sh
@@ -8,19 +8,19 @@
 # ----------
 
 # Location where processing scripts are installed.
-export COMMON_DIR=/usr/local/share/opencog/learn/run-common
-export COMMON_DIR=/home/ubuntu/run-common
+# export COMMON_DIR=/usr/local/share/opencog/learn/run-common
+export COMMON_DIR=/home/ubuntu/src/learn/run-common
 
 # Location where the text corpus and the dictionary files are located.
 # This allows different corpora to be used in different experiments.
-export TEXT_DIR=/home/ubuntu/text/expt-42
+export TEXT_DIR=/home/ubuntu/text/
 
 # Location where the RocksDB databases are kept. This allows different
 # experiments to re-use the same filenames, changing only the directory.
 # It is recommended that this be located on an SSD disk, for
 # performance.
 # If you are using Postgres, just delete `ROCKS_DATA_DIR`.
-export ROCKS_DATA_DIR=/home/ubuntu/data/expt-42
+export ROCKS_DATA_DIR=/home/ubuntu/data/
 
 # Directory in which configuration parameters (including this file)
 # are located. Obtained automatically; don't change.
@@ -29,18 +29,12 @@ export CONFIG_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 # The master config file, which is this file.  Don't change.
 export MASTER_CONFIG_FILE=${CONFIG_DIR}/$( basename "${BASH_SOURCE[0]}" )
 
-# File containing artificial-language generation configuration.
-# Change this as desired.
-export GEN_CONF_FILE=$CONFIG_DIR/1-corpus-conf.sh
-
 # File containing pair-counting configuration.
 # Change this as desired.
 export PAIR_CONF_FILE=$CONFIG_DIR/2-pair-conf-en.sh
-export PAIR_CONF_FILE=$CONFIG_DIR/2-pair-conf-fake.sh
 
 # File containing MST-parsing configuration.
 export MST_CONF_FILE=$CONFIG_DIR/3-mst-conf.sh
-export MST_CONF_FILE=$CONFIG_DIR/3-mpg-conf-fake.sh
 
 # File containing grammatical-class configuration
 export GRAM_CONF_FILE=$CONFIG_DIR/4-gram-conf.sh
