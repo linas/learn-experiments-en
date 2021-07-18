2396aef8db784818e1a28d3281653b959a871b0e
Linas Vepštas <linasvepstas@gmail.com>
2021-07-18 01:50:46 +0000

    First pass at clustering

--- run-2/4-gram-conf.sh
+++ run-2/4-gram-conf.sh
@@ -7,10 +7,10 @@
 export HOSTNAME=localhost
 export PORT=19008
 export PROMPT="scheme@(gram-class)"
-export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-fake.conf
+export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
 
 # Location of the database where grammatical classes will be formed.
-export GRAM_DB=${ROCKS_DATA_DIR}/gram-2.rdb
+export GRAM_DB=${ROCKS_DATA_DIR}/r2-gram-shape-40-disinfo-3.0-4.rdb
 export STORAGE_NODE="(RocksStorageNode \"rocks://${GRAM_DB}\")"
 
 # Scheme function that will perform classification.
