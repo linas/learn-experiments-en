44640acc249f9512c574581cc3bdb718db992348
Linas Vepštas <linasvepstas@gmail.com>
2021-07-21 00:46:00 +0000

    Ongoing work, crash and burn

--- run-2/4-gram-conf.sh
+++ run-2/4-gram-conf.sh
@@ -12,7 +12,9 @@ export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
 # Location of the database where grammatical classes will be formed.
 # export GRAM_DB=${ROCKS_DATA_DIR}/r2-gram-shape-40-disinfo-3.0-4.rdb
 # export GRAM_DB=${ROCKS_DATA_DIR}/r2-gram-shape-40-disinfo-5.0-4.rdb
-export GRAM_DB=${ROCKS_DATA_DIR}/r2-gram-shape-40-junk.rdb
+# export GRAM_DB=${ROCKS_DATA_DIR}/r2-gram-shape-40-junk.rdb
+# export GRAM_DB=${ROCKS_DATA_DIR}/r2-gram-shape-40-mifuzz-0-4.rdb
+export GRAM_DB=${ROCKS_DATA_DIR}/r2-gram-shape-40-mifuzz-5-0-4.rdb
 export STORAGE_NODE="(RocksStorageNode \"rocks://${GRAM_DB}\")"
 
 # Scheme function that will perform classification.
@@ -22,4 +24,5 @@ export STORAGE_NODE="(RocksStorageNode \"rocks://${GRAM_DB}\")"
 API="(define psa star-obj)"
 # export GRAM_CLUSTER="${API} (gram-classify-greedy-discrim psa 0.5 4)"
 # export GRAM_CLUSTER="${API} (gram-classify-greedy-fuzz psa 0.65 0.3 4)"
-export GRAM_CLUSTER="${API} (gram-classify-greedy-disinfo psa 3.0 4)"
+# export GRAM_CLUSTER="${API} (gram-classify-greedy-mifuzz psa 3.0 0.0 4)"
+export GRAM_CLUSTER="${API} (gram-classify-greedy-mifuzz psa 5.0 0 4)"
