e0c8b5c915ee24606818d5455873a066dda60f8e
Linas Vepštas <linasvepstas@gmail.com>
2021-07-22 15:41:09 +0000

    Start MPG processing

--- run-1/3-mpg-conf-en.sh
+++ run-1/3-mpg-conf-en.sh
@@ -1,14 +1,12 @@
 #! /bin/bash
 #
-# Configuration parameters for Planar MST parsing.
+# Configuration parameters for Planar MPG parsing.
 #
-# export CORPORA_DIR=$TEXT_DIR/pair-counted-tranche-1
-export CORPORA_DIR=$TEXT_DIR/beta-tranche-1
+export CORPORA_DIR=$TEXT_DIR/pair-counted-tranche-1
 
 # Directories where in-process and completed files will be moved.
 export IN_PROCESS_DIR=mpg-split
-# export COMPLETED_DIR=mpg-done-tranche-1
-export COMPLETED_DIR=beta-mpg-done-t1
+export COMPLETED_DIR=mpg-done-tranche-1
 
 export SENTENCE_SPLIT=true
 export SPLIT_LANG=en
@@ -23,7 +21,7 @@ export COGSERVER_CONF=${CONFIG_DIR}/3-cogserver/cogserver-mst-en.conf
 export OBSERVE="observe-mpg"
 
 # Location of the database where disjunct counts will be accumulated
-export MST_DB=${ROCKS_DATA_DIR}/mpg_parse.rdb
+export MST_DB=${ROCKS_DATA_DIR}/run-1-en_mpg-tranche-1.rdb
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
 
 # Message printed for each processed file
