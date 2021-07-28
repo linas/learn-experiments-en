ff77c0968066f21ce095d24db5e93088eead9364
Linas Vepštas <linasvepstas@gmail.com>
2021-07-28 20:40:04 +0000

    Ongoing data processing

--- run-1/3-mpg-conf-en.sh
+++ run-1/3-mpg-conf-en.sh
@@ -3,14 +3,16 @@
 # Configuration parameters for Planar MPG parsing.
 #
 # export CORPORA_DIR=$TEXT_DIR/pair-counted-tranche-1
-# export CORPORA_DIR=$TEXT_DIR/pair-counted-tranche-2
-export CORPORA_DIR=$TEXT_DIR/pair-counted-tranche-3
+export CORPORA_DIR=$TEXT_DIR/pair-counted-tranche-2
+# export CORPORA_DIR=$TEXT_DIR/pair-counted-tranche-3
+# export CORPORA_DIR=$TEXT_DIR/pair-counted-tranche-4
 
 # Directories where in-process and completed files will be moved.
 export IN_PROCESS_DIR=mpg-split
 # export COMPLETED_DIR=mpg-done-tranche-1
-# export COMPLETED_DIR=mpg-done-tranche-2
-export COMPLETED_DIR=mpg-done-tranche-3
+export COMPLETED_DIR=mpg-done-tranche-2
+# export COMPLETED_DIR=mpg-done-tranche-3
+# export COMPLETED_DIR=mpg-done-tranche-4
 
 export SENTENCE_SPLIT=true
 export SPLIT_LANG=en
@@ -28,7 +30,8 @@ export OBSERVE="observe-mpg"
 # export MST_DB=${ROCKS_DATA_DIR}/run-1-en_mpg-tranche-1.rdb
 # export MST_DB=${ROCKS_DATA_DIR}/run-1-en_mpg-tranche-12.rdb
 # export MST_DB=${ROCKS_DATA_DIR}/run-1-en_mpg-tranche-123.rdb
-export MST_DB=${ROCKS_DATA_DIR}/run-1-en_mpg-tranche-1234.rdb
+# export MST_DB=${ROCKS_DATA_DIR}/run-1-en_mpg-tranche-1234.rdb
+export MST_DB=${ROCKS_DATA_DIR}/run-1-en_mpg-tranche-12345.rdb
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
 
 # Message printed for each processed file
