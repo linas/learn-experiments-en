0d5f59afcc873f4f67c61cd743dcd565b91e5775
Linas Vepštas <linasvepstas@gmail.com>
2021-09-03 20:56:39 +0000

    Look for idempotency in filtering

--- run-4/4-gram-conf-en.sh
+++ run-4/4-gram-conf-en.sh
@@ -10,4 +10,13 @@ export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
 
 # Location of the database containing MPG pairs
 export MST_DB=${ROCKS_DATA_DIR}/r4-mpg-marg.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r4-zfil-10-4-2-pass-1.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r4-zfil-10-4-2-pass-2.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r4-zfil-10-4-2-pass-3.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r4-zfil-10-4-2-pass-4.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r4-zfil-10-4-2-pass-5.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r4-zfil-10-4-2-pass-6.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r4-zfil-10-4-2-pass-7.rdb
+
+export MST_DB=${ROCKS_DATA_DIR}/r4-mpg-marg.rdb
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
