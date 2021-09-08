f836bdd092715cadded2cff468da3f9c4c9b39bc
Linas Vepštas <linasvepstas@gmail.com>
2021-09-08 16:14:07 +0000

    Get set to trim word-pairs datasets

---
+++ run-5/2-pair-conf-en.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+# Configuration parameters for English word-pair counting.
+# See `2-pair-conf.sh` for documentation of these parameters.
+# ------------
+
+# IPv4 hostname and port number of where the cogserver is running.
+export HOSTNAME=localhost
+export PORT=17005
+export PROMPT="scheme@(en-pairs)"
+export COGSERVER_CONF=${CONFIG_DIR}/2-cogserver/cogserver-pairs-en.conf
+
+# Location of the trimmed database
+export PAIRS_DB=${ROCKS_DATA_DIR}/run-1-prs1-trim-1-1-1.rdb
+
+export STORAGE_NODE="(RocksStorageNode \"rocks://${PAIRS_DB}\")"
