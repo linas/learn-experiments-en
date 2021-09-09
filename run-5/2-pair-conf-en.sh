29864bd880335bd7a400c043ca308d22e18c01f9
ubuntu <ubuntu@learn-en.(none)>
2021-09-09 04:14:00 +0000

    More word-pair trim experiments

--- run-5/2-pair-conf-en.sh
+++ run-5/2-pair-conf-en.sh
@@ -13,5 +13,9 @@ export COGSERVER_CONF=${CONFIG_DIR}/2-cogserver/cogserver-pairs-en.conf
 # Location of the trimmed database
 export PAIRS_DB=${ROCKS_DATA_DIR}/r5-prs1-trim-1-1-1.rdb
 export PAIRS_DB=${ROCKS_DATA_DIR}/r5-prs1-trim-5-5-2.rdb
+export PAIRS_DB=${ROCKS_DATA_DIR}/r5-prs1-trim-10-10-4.rdb
+export PAIRS_DB=${ROCKS_DATA_DIR}/r5-prs1-trim-20-20-6.rdb
+export PAIRS_DB=${ROCKS_DATA_DIR}/r5-prs1-trim-40-40-8.rdb
+export PAIRS_DB=${ROCKS_DATA_DIR}/r5-prs1-trim-80-80-12.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${PAIRS_DB}\")"
