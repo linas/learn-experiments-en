f7e1cf4e9485198e077fe29e704c991fc9f30193
Linas Vepštas <linasvepstas@gmail.com>
2021-09-12 05:28:17 +0000

    Start the next experiment - run-6

---
+++ run-6/4-gram-conf-en.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# Configuration parameters for disjunct distributions.
+#
+# IPv4 hostname and port number of where the cogserver is running.
+export HOSTNAME=localhost
+export PORT=19006
+export PROMPT="scheme@(run-4)"
+export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
+
+# Location of the database containing MPG pairs
+export MST_DB=${ROCKS_DATA_DIR}/r6-stuff.rdb
+
+export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
