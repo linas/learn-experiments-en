5c159a6a1a527ace4eeba69f1040bcf37c3978a1
ubuntu <ubuntu@learn-en.(none)>
2021-09-13 00:16:46 +0000

    Start a README for run-6

--- run-6/4-gram-conf-en.sh
+++ run-6/4-gram-conf-en.sh
@@ -5,10 +5,10 @@
 # IPv4 hostname and port number of where the cogserver is running.
 export HOSTNAME=localhost
 export PORT=19006
-export PROMPT="scheme@(run-4)"
+export PROMPT="scheme@(run-6)"
 export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
 
 # Location of the database containing MPG pairs
-export MST_DB=${ROCKS_DATA_DIR}/r6-stuff.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r6-similarity.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
