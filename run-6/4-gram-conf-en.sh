322809908aadc8af1d86c84129c399c25cb03f09
Linas Vepštas <linasvepstas@gmail.com>
2021-09-21 16:33:00 +0000

    Redo

--- run-6/4-gram-conf-en.sh
+++ run-6/4-gram-conf-en.sh
@@ -10,5 +10,6 @@ export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
 
 # Location of the database containing MPG pairs
 export MST_DB=${ROCKS_DATA_DIR}/r6-similarity.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r6-similarity-tsup.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
