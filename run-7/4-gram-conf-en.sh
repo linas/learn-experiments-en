3b0d983365ad5439d7f43da729d088caac3e34bb
Linas Vepštas <linasvepstas@gmail.com>
2021-09-26 15:31:08 +0000

    Start setting up run 7

---
+++ run-7/4-gram-conf-en.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#
+# Configuration parameters for disjunct distributions.
+#
+# IPv4 hostname and port number of where the cogserver is running.
+export HOSTNAME=localhost
+export PORT=19007
+export PROMPT="scheme@(run-7)"
+export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
+
+# Location of the database containing MPG pairs
+export MST_DB=${ROCKS_DATA_DIR}/r6-similarity-tsup.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r7-merge.rdb
+
+export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
