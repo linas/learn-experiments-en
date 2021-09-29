be602a019ea1f0cf0edaf7294a9b9c11277d1270
Linas Vepštas <linasvepstas@gmail.com>
2021-09-29 15:53:22 +0000

    Take a look at similarity-shape

--- run-6/4-gram-conf-en.sh
+++ run-6/4-gram-conf-en.sh
@@ -11,5 +11,6 @@ export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
 # Location of the database containing MPG pairs
 export MST_DB=${ROCKS_DATA_DIR}/r6-similarity.rdb
 export MST_DB=${ROCKS_DATA_DIR}/r6-similarity-tsup.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r6-similarity-shape.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
