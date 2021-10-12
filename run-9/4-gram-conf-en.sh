a5ba214416e6956ac307811a986cba012b61d887
ubuntu <ubuntu@learn-en.(none)>
2021-10-12 02:13:25 +0000

    Ongoing hackery on expt 9

--- run-9/4-gram-conf-en.sh
+++ run-9/4-gram-conf-en.sh
@@ -12,6 +12,7 @@ export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
 export MST_DB=${ROCKS_DATA_DIR}/r9-merge-a.rdb
 export MST_DB=${ROCKS_DATA_DIR}/run-1-t1234-shape.rdb
 export MST_DB=${ROCKS_DATA_DIR}/r9-merge-b.rdb
-export MST_DB=${ROCKS_DATA_DIR}/r9-merge-y.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r9-merge-e.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r9-merge-g.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
