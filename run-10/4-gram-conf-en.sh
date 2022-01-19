7bd886efe3115bbff1e5291f11a77906fade5c5c
ubuntu <ubuntu@learn-en.(none)>
2022-01-19 18:38:51 +0000

    Lets look at entropies, too

--- run-10/4-gram-conf-en.sh
+++ run-10/4-gram-conf-en.sh
@@ -10,10 +10,12 @@ export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-en.conf
 
 # Location of the database containing MPG pairs
 export MST_DB=${ROCKS_DATA_DIR}/run-1-t1234-shape.rdb
-export MST_DB=${ROCKS_DATA_DIR}/r10-dbg-a.rdb
-export MST_DB=${ROCKS_DATA_DIR}/r10-dbg-b.rdb
-export MST_DB=${ROCKS_DATA_DIR}/r10-dbg-c.rdb
 export MST_DB=${ROCKS_DATA_DIR}/r10-merge.rdb
-export MST_DB=${ROCKS_DATA_DIR}/r10-singles.rdb
+# export MST_DB=${ROCKS_DATA_DIR}/r10-singles.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r10-mrg-q0.6-c0.3.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r10-mrg-q0.7-c0.4.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r10-mrg-q0.8-c0.5.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r9-sim-200+entropy.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r9-sim-200+mi.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
