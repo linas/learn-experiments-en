7fce9f4ebd59d300d2d20884fe8f2b56e845cbed
ubuntu <ubuntu@learn-en.(none)>
2022-01-20 05:53:56 +0000

    Stuff about MI again

--- run-10/4-gram-conf-en.sh
+++ run-10/4-gram-conf-en.sh
@@ -17,5 +17,6 @@ export MST_DB=${ROCKS_DATA_DIR}/r10-mrg-q0.7-c0.4.rdb
 export MST_DB=${ROCKS_DATA_DIR}/r10-mrg-q0.8-c0.5.rdb
 export MST_DB=${ROCKS_DATA_DIR}/r9-sim-200+entropy.rdb
 export MST_DB=${ROCKS_DATA_DIR}/r9-sim-200+mi.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r10-mrg-q0.5-c0.2.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
