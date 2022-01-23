f9e0504e868acd677fa0b0c6e77e2dee0967e3ff
ubuntu <ubuntu@learn-en.(none)>
2022-01-23 19:05:54 +0000

    Ooops operational mistakes

--- run-11/4-gram-conf-en.sh
+++ run-11/4-gram-conf-en.sh
@@ -7,9 +7,13 @@ export HOSTNAME=localhost
 export PORT=19011
 export PROMPT="scheme@(run-11)"
 export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-n4.conf
+export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-n3.conf
+export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-n2.conf
 
 # Location of the database containing MPG pairs
+export MST_DB=${ROCKS_DATA_DIR}/r9-sim-200+mi.rdb
 export MST_DB=${ROCKS_DATA_DIR}/r11-mrg-q0.7-c0.2-n4.rdb
-export MST_DB=${ROCKS_DATA_DIR}/r9-sim-200+entropy.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r11-mrg-q0.7-c0.2-n3.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r11-mrg-q0.7-c0.2-n2.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
