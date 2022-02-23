4e9cc23ebd7489393da633689f2cff73d72bbccd
ubuntu <ubuntu@learn-en.(none)>
2022-02-23 16:38:57 +0000

    Ongoing turbulence with crashed data

--- run-13/2-pair-conf-en.sh
+++ run-13/2-pair-conf-en.sh
@@ -8,6 +8,7 @@
 export HOSTNAME=localhost
 export PORT=17013
 export PROMPT="scheme@(en-pairs)"
+export PROMPT="scheme@(r3-marg)"
 export COGSERVER_CONF=${CONFIG_DIR}/2-cogserver/cogserver-pairs-en.conf
 
 # Location of the raw pairs database
@@ -15,5 +16,6 @@ export PAIRS_DB=${ROCKS_DATA_DIR}/run-1-marg-tranche-1234.rdb
 export PAIRS_DB=${ROCKS_DATA_DIR}/run-1-en_pairs-tranche-1234.rdb
 export PAIRS_DB=${ROCKS_DATA_DIR}/run-1-en_pairs-tranche-123.rdb
 export PAIRS_DB=${ROCKS_DATA_DIR}/run-1-marg-tranche-123.rdb
+export PAIRS_DB=${ROCKS_DATA_DIR}/r3-mpg-marg.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${PAIRS_DB}\")"
