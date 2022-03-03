8c62a7cf0329ee1ddb7e662a8a2526acb74b969d
ubuntu <ubuntu@learn-en.(none)>
2022-03-03 19:34:18 +0000

    Crap. Incomplete/incorrect entrop calcs

--- run-14/4-gram-conf-en.sh
+++ run-14/4-gram-conf-en.sh
@@ -8,21 +8,21 @@ export PORT=19014
 export PORT=20014
 # export PORT=20114
 # export PORT=20214
-# export PORT=20314
+export PORT=20314
 # export PORT=20414
 
 export PROMPT="(r14)"
 export PROMPT="(r14-i0)"
 # export PROMPT="(r14-i1)"
 # export PROMPT="(r14-i2)"
-# export PROMPT="(r14-i3)"
+export PROMPT="(r14-i3)"
 # export PROMPT="(r14-i4)"
 
 export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram.conf
 export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-i0.conf
 # export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-i1.conf
 # export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-i2.conf
-# export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-i3.conf
+export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-i3.conf
 # export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram-i4.conf
 
 # Location of the database containing MPG pairs
@@ -30,7 +30,7 @@ export MST_DB=${ROCKS_DATA_DIR}/r14-sim200.rdb
 export MST_DB=${ROCKS_DATA_DIR}/r14-imp-q0.7-c0.9-n0.rdb
 # export MST_DB=${ROCKS_DATA_DIR}/r14-imp-q0.7-c0.9-n1.rdb
 # export MST_DB=${ROCKS_DATA_DIR}/r14-imp-q0.7-c0.9-n2.rdb
-# export MST_DB=${ROCKS_DATA_DIR}/r14-imp-q0.7-c0.9-n3.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r14-imp-q0.7-c0.9-n3.rdb
 # export MST_DB=${ROCKS_DATA_DIR}/r14-imp-q0.7-c0.9-n4.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
