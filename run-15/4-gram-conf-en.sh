d448fc96c52622e7dc8eeaf6768cb6802fa15f48
Linas Vepstas <linasvepstas@gmail.com>
2023-03-07 18:35:19 +0000

    More sim computations

--- run-15/4-gram-conf-en.sh
+++ run-15/4-gram-conf-en.sh
@@ -5,13 +5,17 @@
 # IPv4 hostname and port number of where the cogserver is running.
 export HOSTNAME=localhost
 export PORT=19015
+# export PORT=20015
 
-export PROMPT="(r15)"
+export PROMPT="(r15-500)"
+export PROMPT="(r15-6k)"
 
 export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-gram.conf
+# export COGSERVER_CONF=${CONFIG_DIR}/4-cogserver/cogserver-sim500.conf
 
 # Location of the database containing MPG pairs
 export MST_DB=${ROCKS_DATA_DIR}/r15-sim500.rdb
-export MST_DB=${ROCKS_DATA_DIR}/r15-sim2500.rdb
+# export MST_DB=${ROCKS_DATA_DIR}/r15-sim2500.rdb
+export MST_DB=${ROCKS_DATA_DIR}/r15-sim6000.rdb
 
 export STORAGE_NODE="(RocksStorageNode \"rocks://${MST_DB}\")"
